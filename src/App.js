396a262316757945f8af0c37fcb29d6fe3fde484
ugochukwu onyekanma <ugochikanele@gmail.com>
2022-11-18 10:14:04 +0100

    cart and modal reducers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
+import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotal, getCartItems } from './features/cart/cartSlice';
+import Modal from './components/Modal';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const { cartItems, isLoading } = useSelector((state) => state.cart)
+  const { isOpen } = useSelector((state) => state.modal)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+      dispatch(calculateTotal())
+  },[cartItems])
+
+  useEffect(() => {
+      dispatch(getCartItems())
+  },[])
+
+  if(isLoading) {
+    return <div className='loading'>
+      <h1>loading...</h1>
     </div>
+  }
+
+  return (
+    <main>
+      {isOpen && <Modal />}      
+      <Navbar />
+      <CartContainer />
+    </main>
   );
 }
 
---
+++ src/components/CartContainer.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import CartItem from './CartItem'
+import { openModal } from '../features/modal/modalSlice'
+
+const CartContainer = () => {
+    const dispatch = useDispatch()
+    const { amount, total, cartItems } = useSelector((state) => state.cart)
+
+    if(amount < 1) {
+        return (
+            <section className='cart'>
+                {/* cart header */}
+                <header>
+                    <h2>Your Bag</h2>
+                    <h4 className='empty-cart'>is currently empty</h4>
+                </header>
+            </section>
+        )
+    }
+    return (
+        <section className='cart'>
+            {/* cart header */}
+            <header>
+                <h2>Your Bag</h2>
+            </header>
+            {/* cart items */}
+            <div>
+                {cartItems.map((item) => {
+                    return <CartItem key={item.id} {...item} />
+                })}
+            </div>
+            <footer>
+                <hr />
+                <div className='cart-total'>
+                    <h4>
+                        total <span>${total.toFixed(2)}</span>
+                    </h4>
+                </div>
+                <button className='btn clear-btn' onClick={() => dispatch(openModal())}>clear cart</button>
+            </footer>
+        </section>
+    )
+}
+
+export default CartContainer
